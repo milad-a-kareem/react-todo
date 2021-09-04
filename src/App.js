62f1a9daa08197525061f906354154fcf1e391db
Milad <milad.kareem@gmail.com>
2021-09-04 04:31:55 +0300

    done!!

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
 import './App.css';
-import {useState} from 'react'
+import { useState , useEffect} from 'react'
 import Header from './Header/Header';
 import List from './List/List';
 
 function App() {
   let myStorage = window.localStorage
-  let todos = myStorage.getItem('todos')
-  const [todoState, setTodoState] = useState([
-    
-  ])
+  let todos
+  const [todoState, setTodoState] = useState([])
 
-  
+  useEffect(()=>{
+    todos = myStorage.getItem('todos')
+    if(todos){
+      setTodoState(JSON.parse(todos))
+    }
+  }, [])
+
+  useEffect(()=>{
+    myStorage.setItem('todos', JSON.stringify(todoState))
+  }, [todoState])
 
   const addHandler = (todoText)=>{
     if(todoText.length>0){
       let newTodos = [...todoState]
       newTodos.unshift({id: Math.floor(Math.random()*1000), text: todoText, done:false})
       setTodoState(newTodos)
-      myStorage.setItem('todos', JSON.stringify(newTodos))
-      todos = myStorage.getItem('todos')
+      // myStorage.setItem('todos', JSON.stringify(newTodos))
+      // todos = myStorage.getItem('todos')
     }
   }
 
   const delHandler = (id)=>{
     const newTodos  = todoState.filter( todo => { return todo.id !== id})
     setTodoState(newTodos)
-    myStorage.setItem('todos', JSON.stringify(newTodos))
-    todos = myStorage.getItem('todos')
+    // myStorage.setItem('todos', JSON.stringify(newTodos))
+    // todos = myStorage.getItem('todos')
   }
   const changeHandler = (id , s)=>{
     let newTodos  = [...todoState]
@@ -42,20 +49,14 @@ function App() {
     a.then(res=>{
       if (res){
         setTodoState(newTodos)
-        myStorage.setItem('todos', JSON.stringify(newTodos))
-        todos = myStorage.getItem('todos')
+        // myStorage.setItem('todos', JSON.stringify(newTodos))
+        // todos = myStorage.getItem('todos')
       }
     })
 
   }
 
-  if(todos){
-    setTimeout(()=>{
-      todos = myStorage.getItem('todos')
-      JSON.parse(todos) !== todoState ? setTodoState(JSON.parse(todos)):
-      console.log(JSON.parse(todos))
-    }, 100)
-  }
+  
   
   return (
     <div className="App">
